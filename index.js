0b649e3d4fe7325786bb65d9f0973b27d7e8f70e
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2022-07-19 10:55:49 +0000

    Fix that annoying errors

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const aoi = require("aoi.js")
-const bot = new aoi.Bot({
-     token: process.env.token,
-     prefix: [c?]
-});
+const aoijs = require("aoi.js")
 
-bot.onMessage({
-  guildOnly: false //make commands work in dms
+const bot = new aoijs.Bot({
+token: process.env['token'],
+prefix: "c?",
+intents: ["GUILDS", "GUILD_MESSAGES"]
+})
+bot.onMessage();
+bot.command({
+name: "ping",
+code: `Pong! $pingms`
 });
-bot.onMusicStart()
-bot.musicStartCommand({
- channel: "$channelID", 
- code: `
- $color[RANDOM]
- $author[Now Playing]
- $description[Playing $songInfo[title]]`
-});
-bot.musicEndCommand({
-        channel: "$channelID",
-        code: `$sendMessage[{description: I'm leaving the Voice Channel since no one is playing music anymore}{delete:5s};no]`
-        });
-
-bot.loadCommands('./Commmands/'); //command handler
 bot.status ({
  text: "c?help",
  type: "LISTENING",
  time: 10
-});
+})
+const loader = new aoijs.LoadCommands(bot)
+loader.load(bot.cmd,"./Commands/")
