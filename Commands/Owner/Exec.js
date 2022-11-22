ccfffae6d57217e19a608d526c9227488ef1ba89
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2022-11-22 19:14:47 +0300

    Update Exec.js

--- Commands/Owner/Exec.js
+++ Commands/Owner/Exec.js
@@ -1,5 +1,5 @@
 module.exports = ({
   name: "exec",
   $if: "v4",
-  code: `$exec[$message] $log[Команда $message выполняется!!!] $addcmdreactions[✅] $onlyif[$authorid==802951312873750578] ` //Change id in onlyif to your user id
+  code: `$onlyif[$authorid==802951312873750578] $exec[$message] $log[Команда $message выполняется!!!] $addcmdreactions[✅]  ` //Change id in onlyif to your user id
 })
