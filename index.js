ce3071812b78343865785639463f18bea555f1a4
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2022-11-23 11:53:40 +0000

    Secure something + aoi.js v6 and aoi.panel 0.0.3 update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {Panel} = require("@akarui/aoi.panel")
 const bot = new aoijs.Bot({
 token: process.env['token'],
 prefix: "c?",
-intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"],
+intents: ["Guilds", "GuildMessages", "GuildVoiceStates", "MessageContent"],
 mobile: true
 })
 
@@ -26,8 +26,8 @@ const voice = new aoijs.Voice(
 
 voice.onTrackStart();
 
-bot.onJoin();
-bot.onMessage();
+bot.addEvent("onJoin");
+bot.addEvent("onMessage");
 
 bot.command({
 name: "ping",
@@ -40,25 +40,25 @@ bot.status ({
  time: 10
 })
 
-const loader = new aoijs.LoadCommands(bot)
-loader.load(bot.cmd,"./Commands/")
-loader.load(voice.cmd, "./Voice/"); //voice cmds
+bot.commands.load("./Commands/")
 
 bot.variables({
 updates: "Обновлений пока нету!",
 enabledwelcomer: false,
 })
 
-//console.log("-------- Website --------");
-//const panel = new Panel({
-//    username: "TDR",//username for logging in
-//    password: process.env['pass'],//password for logging in
-//    secret: "aoijs",//session secret, i'll edit it later, because now its not safe
-//    port: 3000,//port on which website is hosted, Not required! Default 3000
-//    bot: bot,//your aoi.js client
-//    mainFile: "index.js",//Main file where code is running.Not required, default taken from package.json
-//    commands: "./Commands/"// folder name in which all the edit needing files are there.
-//})
-//panel.loadPanel()//Load The Panel
+console.log("-------- Website --------");
+const panel = new Panel({
+    username: "TDR",//username for logging in
+    password: process.env['pass'],//password for logging in
+    secret: require('crypto').randomBytes(16).toString("hex"),
+    port: 3000,//port on which website is hosted, Not required! Default 3000
+    bot: bot,//your aoi.js client
+    mainFile: "index.js",//Main file where code is running.Not required, default taken from package.json
+    commands: "./Commands/"// folder name in which all the edit needing files are there.
+})
+panel.loadPanel()//Load The Panel
 
-//panel.onError()
+panel.onError()
+
+bot.start()
