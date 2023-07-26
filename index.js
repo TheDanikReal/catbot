9a7c73ecbdfb64338351f74c6223980decc05ab1
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2023-07-26 17:49:09 +0000

    I'm back to developing project!

--- index.js
+++ index.js
@@ -1,33 +1,64 @@
-const aoijs = require("aoi.js")
-const {Voice, LoadCommands, Bot } = require("aoi.js")
 const {Panel} = require("@akarui/aoi.panel")
+const { AoiClient, LoadCommands } = require("aoi.js");
+
+const bot = new AoiClient({
+    token: process.env['token'],
+    prefix: "c?",
+    intents: ["MessageContent", "Guilds", "GuildMessages"],
+    events: ["onMessage", "onInteractionCreate"],
+    database: {
+        type: "aoi.db",
+        db: require("@akarui/aoi.db"),
+        tables: ["main"],
+        path: "./database/"
+    }
+});
+/*
+
+waiting for update of library 
+*/
+const loader = new LoadCommands(bot);
+loader.load(bot.cmd, "./Commands/");
+
+
+
+const panel = new Panel({
+  username:["TDR"],
+  password:process.env['pass'],
+  secret: require('crypto').randomBytes(16).toString("hex"),
+  commands: "./Commands/",
+  port:3000,
+  bot: bot
+})
+/*
+panel.loadAPI({
+  auth: require('crypto').randomBytes(16).toString("hex")
+})
+
+panel.loadGUI({
+  username:["TDR"],
+  password:[process.env['pass']],
+})
+*/
+
+/*
+
+
+const aoijs = require("aoi.js");
+const {LoadCommands, Bot } = require("aoi.js");
+const {Panel} = require("@akarui/aoi.panel");
 
 const bot = new aoijs.Bot({
 token: process.env['token'],
 prefix: "c?",
-intents: ["Guilds", "GuildMessages", "GuildVoiceStates", "MessageContent"],
+intents: ["GUILDS", "GUILDMESSAGES", "GUILDVOICESTATES", "MESSAGECONTENT"],
 mobile: true
-})
-
-const voice = new aoijs.Voice(
-  bot,
-  {
-    cache: {
-      cacheType: "Memory",//Disk
-      enabled: true,
-      //directory : "music", only for Disk type
-    },
-  playerOptions: {
-    trackInfoInterval: 5000,
-  },//optional
-  },
-  true, //to enable pruneMusic 
-);
+});
 
 voice.onTrackStart();
 
-bot.addEvent("onJoin");
-bot.addEvent("onMessage");
+bot.onJoin();
+bot.onMessage();
 
 bot.command({
 name: "ping",
@@ -38,14 +69,15 @@ bot.status ({
  text: "c?help",
  type: "PLAYING",
  time: 10
-})
+});
 
-bot.commands.load("./Commands/")
+const loader = new aoijs.LoadCommands(bot)
+loader.load(bot.cmd,"./Commands/")
 
 bot.variables({
 updates: "Обновлений пока нету!",
 enabledwelcomer: false,
-})
+});
 
 console.log("-------- Website --------");
 const panel = new Panel({
@@ -56,9 +88,10 @@ const panel = new Panel({
     bot: bot,//your aoi.js client
     mainFile: "index.js",//Main file where code is running.Not required, default taken from package.json
     commands: "./Commands/"// folder name in which all the edit needing files are there.
-})
-panel.loadPanel()//Load The Panel
+});
+panel.loadPanel();//Load The Panel
 
-panel.onError()
+panel.onError();
 
-bot.start()
+redoing everything from scratch
+*/
--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
 	deps = [
-pkgs.nodejs-16_x
+pkgs.python310
+#pkgs.nodejs-16_x
         pkgs.nodePackages.typescript-language-server
         pkgs.yarn
         pkgs.replitPackages.jest
